524f7415b113401be5fa3d18313637f626a46d17
Peter <peter.martak1@gmail.com>
2025-05-17 15:08:00 +0200

    Create hero with name and start the game

---
+++ src/main/java/sk/peter/Hero.java
@@ -0,0 +1,13 @@
+package sk.peter;
+
+public class Hero {
+    private String name;
+
+    public Hero(String name){
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
---
+++ src/main/java/sk/peter/Main.java
@@ -0,0 +1,14 @@
+package sk.peter;
+
+import java.util.Scanner;
+
+public class Main {
+    private final static Scanner scanner = new Scanner(System.in);
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Gladiator game!");
+        System.out.println("Please enter your name");
+        final String name = scanner.nextLine();
+        final Hero hero = new Hero(name);
+        System.out.println("Hello: " + hero.getName() + " Let's start the game!");
+    }
+}
